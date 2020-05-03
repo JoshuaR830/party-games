1491f055de5f4f5316af48db86187998d336ae5e
Joshua <joshua.richardson@myunidays.com>
2020-05-03 08:36:39 +0100

    Gameplay improvements

--- wwwroot/js/letters.js
+++ wwwroot/js/letters.js
@@ -30,9 +30,7 @@ connection.on("ReceiveCompleteRound", function() {
     stopTimer();
 })
 
-document.querySelector('.js-letters-start').addEventListener('click', function(event) {
-    lettersConnection.invoke("ChooseLetters", connectionName);
-});
+document.querySelector('.js-letters-start').addEventListener('click', launchWordGame);
 
 document.querySelector('#startGame').addEventListener('click', launchWordGame);
 document.querySelector('#play-again-option').addEventListener('click', launchWordGame);
@@ -44,7 +42,7 @@ document.getElementById('playAgain').addEventListener('click', function() {
 function launchWordGame() {
     let timerMins = parseInt(document.getElementById('set-minutes').value);
     let timerSecs = parseInt(document.getElementById('set-seconds').value);
-    
+
     let time = [timerMins, timerSecs];
     connection.invoke("SendTime", connectionName, time)
     lettersConnection.invoke("ChooseLetters", connectionName);
@@ -71,7 +69,24 @@ lettersConnection.on("LettersForGame", function(jsonLetters) {
         // <div class="letter-container letter-choice populated js-letter random-letter-0" data-origin="0">A</div>
         
         let letterContainer = document.createElement('div');
-        letterContainer.className = `letter-container letter-choice populated js-letter random-letter-${i}`;
+        let myName = document.querySelector('#my-name').value;
+        console.log(myName);
+        letterContainer.className = `letter-container letter-choice populated js-letter ${myName==="Andrew"?`random-letter-${i}`:""}`;
+        console.log(myName);
+        if (myName.toLowerCase() === "andrew") {
+            document.querySelector(`.js-random-letters-container`).style = "height: 260px; width: 260px; border-radius: 50%; border: 2px solid #55edba5b; box-sizing: content-box; box-shadow: 0 0 10px #55edba5b;"
+            console.log(chosenLetters[i]);
+            let angle = (2*Math.PI)/chosenLetters.length;
+            let xPos = (260/2) - 30;
+            let yPos = (260/2) - 30;
+            let radius = 100;
+            letterContainer.style = ""
+            let x = Math.round(radius * (Math.sin(i *angle))) + xPos;
+            let y = Math.round(radius * (Math.cos(i *angle))) + yPos;
+            console.log(`${x},${y}`);
+            letterContainer.style = `position: absolute; top: ${y}px; left: ${x}px; margin-top: 0; width: 60px; border-radius: 50%;`;
+            // }
+        }
         letterContainer.setAttribute('data-origin', `${i}`);
         letterContainer.textContent = chosenLetters[i];
         container.appendChild(letterContainer);
@@ -168,40 +183,90 @@ function placeNextLetter(letter, origin) {
     console.log(letterOrigins)
 }
 
+function removeConfirmed()
+{
+    document.querySelectorAll('.js-selected-letter.populated').forEach(function($el) {
+        $el.classList.remove("confirmed");
+        $el.classList.remove("alert-user");
+    })
+}
+
 
 confirmWord.addEventListener('click', function() {
-    var currentWord = letterSpaces.join('');
+
+    
+
+    let currentWord = letterSpaces.join('');
+
+    if (wordsCreated.includes(currentWord)){
+        document.querySelectorAll('.js-selected-letter.populated').forEach(function($el) {
+            $el.classList.add("alert-user");
+        })
+    
+        setTimeout(removeConfirmed, 150);
+        return;
+    }
+
+    document.querySelectorAll('.js-selected-letter.populated').forEach(function($el) {
+        $el.classList.add("confirmed");
+    })
+
+    setTimeout(removeConfirmed, 150);
+
     wordsCreated.push(currentWord);
+    let num = wordsCreated.length - 1;
     var word = document.createElement('div');
     word.textContent = currentWord;
-    word.className = "word-list-item";
+    word.className = `word-list-item word-${num}`;
+
+    word.addEventListener('click', function() {
+        lettersConnection.invoke("WordTicked", currentWord, connectionName);
+    })
+
     wordList.appendChild(word);
 
     lettersConnection.invoke("IsValidWord", currentWord, connectionName);
+
+    var wordListContainer = document.querySelector('.js-word-list');
+    var scrollWidth = wordListContainer.scrollWidth;
+    console.log(scrollWidth);
+    wordListContainer.scrollTo()
+
+    if(wordListContainer.scrollLeft !== scrollWidth) {
+        wordListContainer.scrollTo((scrollWidth), 0);
+    }
 });
 
-lettersConnection.on("WordStatusResponse", function(status) {
-    console.log(status);
+lettersConnection.on("WordStatusResponse", function(status, word) {
+    console.log(status, word);
+    if (status === false) {
+        let wordPos = getWordPosition(word);
+        if(wordPos < 0) {
+            return;
+        }
+        
+        document.querySelector(`.word-${wordPos}`).classList.add("word-error");
+    }
 })
 
+lettersConnection.on("TickWord", function(word) {
+    let wordPos = getWordPosition(word)
+    if(wordPos < 0) {
+        return;
+    }
+    console.log("Word ticked");
+    document.querySelector(`.word-${wordPos}`).classList.add("word-ticked");
+})
 
-// var xhttp = new XMLHttpRequest()
-// xhttp.onreadystatechange = function() {
-//     if(this.readyState === 4 && this.status === 200)
-//     {
-//         console.log("success");
-//     }
+function getWordPosition(word) {
+    let wordPos = wordsCreated.indexOf(word)
+    if (wordPos < 0){
+        console.log("does not contain word");
+        return -1;
+    }
+    return wordPos;
+}
 
-//     if(this.readyState === 4 && this.status === 404)
-//     {
-//         console.log("failed")
-//     }
-// }
-
-// xhttp.open("GET", "https://www.dictionary.com/browse/hello");
-// // xhttp.setRequestHeader('Content-type', 'application/json; charset=utf-8');
-// xhttp.setRequestHeader('Access-Control-Allow-Origin', '*');
-// xhttp.send();
-
-// fetch("https://www.dictionary.com/browse/hello");
-// fetch("https://stackoverflow.com/");
+document.querySelectorAll('.word-list-item').forEach(function($el) {
+    $el.addEventListener()
+})

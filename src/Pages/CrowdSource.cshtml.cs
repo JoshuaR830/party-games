64e76a04e93bedf3cfb90063ed85540b1d88348b
Joshua <joshua.richardson@myunidays.com>
2020-05-19 17:48:34 +0100

    WIP - started adding crowd source, changed some layouts

---
+++ src/Pages/CrowdSource.cshtml.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Chat.Pages
+{
+    public class CrowdSourceModel : PageModel
+    {
+        // GET
+        public void OnGet()
+        {
+            
+        }
+    }
+}

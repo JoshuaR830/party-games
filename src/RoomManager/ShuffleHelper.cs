e78062abb67fd99571f4ed5fe5ffa8f88f26e04c
Joshua <psyjr4@nottingham.ac.uk>
2020-05-23 08:18:40 +0100

    Added list for shuffle helper

---
+++ src/RoomManager/ShuffleHelper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Chat.RoomManager
+{
+    public class ShuffleHelper<T> : IShuffleHelper<T>
+    {
+        public List<T> ShuffleList(List<T> list)
+        {
+            var shuffledList = list;
+            // ToDo: OrderBy random number
+            return shuffledList;
+        }
+    }
+}
---
+++ tests/PartyGamesTests/RoomManager/TopicManagerTests.cs
@@ -0,0 +1,12 @@
+﻿namespace PartyGamesTests.RoomManager
+{
+    public class TopicManagerTests
+    {
+        public void TestTopicManager()
+        {
+            
+            // var topics1 = ;
+            // var topics2 =
+        }
+    }
+}

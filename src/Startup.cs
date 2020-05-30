344e6fb9def6e2af23d510c4303d546a6d47fd9f
Joshua <psyjr4@nottingham.ac.uk>
2020-05-30 11:29:34 +0100

    Added a new pixenary hub

--- src/Startup.cs
+++ src/Startup.cs
@@ -1,12 +1,7 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Chat.GameManager;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -90,6 +85,7 @@ namespace Chat
             {
                 routes.MapHub<ChatHub>("/chatHub");
                 routes.MapHub<LettersHub>("/lettersHub");
+                routes.MapHub<PixenaryHub>("/pixenaryHub");
             });
 
             app.UseMvc();
